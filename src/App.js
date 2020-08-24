6b761bdc2a3360421db197c6c7903d7553798f2a
mdziedzi <marcindziedzic8@gmail.com>
2020-08-24 18:34:31 +0200

    Add current date and jumping in time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import holidays from './holidays'
 
 function App() {
+
+  const [currentDate, setCurrentDate] = useState({ value: new Date() })
+
+  const addDays = (date, days) => {
+    var result = new Date(Number(date));
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+  const toDay = (date) => {
+    return ("0" + date.getDate()).slice(-2)
+  }
+
+  const toMonth = (date) => {
+    return ("0" + (date.getMonth() + 1)).slice(-2)
+  }
+
+  const onNextDayClicked = () => {
+    setCurrentDate({ value: addDays(currentDate.value, 1) })
+  }
+
+  const onPrevDayClicked = () => {
+    setCurrentDate({ value: addDays(currentDate.value, -1) })
+  }
+
+  const onTodayClicked = () => {
+    setCurrentDate({ value: new Date() })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Day: {toDay(currentDate.value)} Month: {toMonth(currentDate.value)}</p>
+        <p>Today's holiday: {holidays[toMonth(currentDate.value)].days[toDay(currentDate.value)]}</p>
+        <p>{new Date(currentDate.value).toISOString()}</p>
+        <button onClick={onNextDayClicked}>next</button>
+        <button onClick={onPrevDayClicked}>prev</button>
+        <button onClick={onTodayClicked}>today</button>
       </header>
     </div>
   );
---
+++ src/holidays.js
@@ -0,0 +1,24 @@
+const holidays = {
+    "08": {
+      days: {
+        "01": ["Narodowy Dzień Pamięci Powstania Warszawskiego (Dzień Pamięci Warszawy)"],
+        "02": ["Narodowy Dzień Pamięci Powstania Warszawskiego (Dzień Pamięci Warszawy)"],
+        "03": ["Narodowy Dzień Pamięci Powstania Warszawskiego (Dzień Pamięci Warszawy)"],
+        "04": ["Narodowy Dzień Pamięci Powstania Warszawskiego (Dzień Pamięci Warszawy)"],
+        "23": ["aaaa"],
+        "24": ["bbb"],
+        "25": ["ccc"],
+      }
+    },
+    "09": {
+      days: {
+        "01": [""],
+        "02": [""],
+        "03": [""],
+        "04": [""]
+      }
+
+    }
+  }
+  
+  export default holidays
