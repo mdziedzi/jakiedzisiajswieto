3ddd0009d7990e0c5192bcae8abef0d433b11e3d
mdziedzi <marcindziedzic8@gmail.com>
2020-09-02 17:12:18 +0200

    Add multi colour background

--- src/App.js
+++ src/App.js
@@ -43,16 +43,38 @@ function App() {
     setCurrentDate({ value: new Date() })
   }
 
+  function generate() {
+
+    var hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
+    
+    function populate(a) {
+      for ( var i = 0; i < 6; i++ ) {
+        var x = Math.round( Math.random() * 14 );
+        var y = hexValues[x];
+        a += y;
+      }
+      return a;
+    }
+    
+    var newColor1 = populate('#');
+    var newColor2 = populate('#');
+    var angle = Math.round( Math.random() * 360 );
+
+    return "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
+  }
+
   return (
     <>
-      <div className="Page">
+      <div className="Page" style={{background: generate()}}>
         <div className="Container">
           <div className="Clickable ArrowColumn" onClick={onPrevDayClicked}><img src="arrow.svg" alt="Previous day" className="LeftArrow"></img></div>
           <div className="MainColumn">
             <h1 className="Header Clickable" onClick={onTodayClicked}>Jakie dzisiaj święto?</h1>
-            <div className="Holiday">
+            <div className="HolidayContainer">
+              <div className="HolidayArea">
               <p className="Date">{translateWeekDay(currentDate.value)}, {toDay(currentDate.value)} {translateMonth()} {currentDate.value.getFullYear()}</p>
-              <p>{holidays[toMonth(currentDate.value)][toDay(currentDate.value)]}</p>
+              <p className="Holiday">{holidays[toMonth(currentDate.value)][toDay(currentDate.value)]}</p>
+              </div>
             </div>
           </div>
           <div className="Clickable ArrowColumn" onClick={onNextDayClicked}><img src="arrow.svg" alt="Previous day" className="RightArrow"></img></div>
